27256869c64f5c5cc4187d2dce3ad9db0f4f32cb
LasseStaus <l.stausgaard@gmail.com>
2025-05-26 19:18:44 +0200

    forgot basepath

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -1,12 +1,18 @@
 "use client";
 import { useEffect } from "react";
 
+function getBasePath(): string {
+  const path = window.location.pathname;
+  return path.startsWith("/ereolen") ? "/ereolen" : "";
+}
+
 export default function NotFound() {
   useEffect(() => {
     const { pathname, search } = window.location;
 
-    const isLegacy = /^\/+ting\/+object\/+[^/?#]+\/?$/.test(pathname);
-    const base = process.env.NODE_ENV === "production" ? "/ereolen" : "";
+    const isLegacy = /\/ting\/object\/[^/?#]+\/?$/.test(pathname);
+
+    const base = getBasePath();
 
     const redirectTo = isLegacy
       ? `${base}/?from=${encodeURIComponent(pathname + search)}`
