1bc22f3d5af09be2618abd639b6a0fdf3b402391
LasseStaus <l.stausgaard@gmail.com>
2025-05-26 16:32:23 +0200

    Fix catchall redirect.

--- app/[...catchall]/page.tsx
+++ app/[...catchall]/page.tsx
@@ -1,16 +1,13 @@
 "use client";
-import { buildRedirectParam } from "@/lib/utils";
 import { useEffect } from "react";
+import { buildRedirectParam } from "@/lib/utils";
 
 export default function CatchAll() {
   useEffect(() => {
-    if (typeof window === "undefined") return;
-
     const redirectTo = buildRedirectParam(
       window.location.pathname,
       window.location.search
     );
-
     window.location.replace(redirectTo);
   }, []);
 
