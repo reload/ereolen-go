e5b8c8ca81bb0b364e31a3b836ca1016021007b4
Kasper Birch <kasperbirch1@gmail.com>
2025-07-02 11:52:05 +0200

    Update H1 layout to support responsive

--- app/page.tsx
+++ app/page.tsx
@@ -9,7 +9,7 @@ export default function Home() {
   return (
     <div className="grid h-full w-full flex-1 grid-rows-[auto_min-content] items-center gap-8">
       <div className="mx-auto grid w-full justify-center space-y-4 py-12">
-        <h1 className="text-primary flex items-center justify-center gap-4 text-center text-6xl font-extrabold">
+        <h1 className="text-primary flex flex-col items-center justify-center gap-4 text-center text-6xl font-extrabold sm:flex-row">
           <Image
             src={addBasePath("/logo-red.png")}
             alt="EReolen Logo"
