0be896478e0a4f9f3f1403a67e32efb6af007c49
Kasper Birch <kasperbirch1@gmail.com>
2025-07-04 13:23:20 +0200

    Fix spacing and centering for headline

--- app/page.tsx
+++ app/page.tsx
@@ -7,15 +7,11 @@ export default function Home() {
   return (
     <div className="grid h-full w-full flex-1 grid-rows-[auto_min-content] items-center gap-8">
       <div className="mx-auto grid w-full justify-center space-y-4 py-12">
-        <Typography
-          variant={"h1"}
-          as={"h1"}
-          className="flex items-center justify-center gap-4 text-center"
-        >
+        <Typography variant={"h1"} as={"h1"} className="md:text-center">
           eReolen GO er flyttet
         </Typography>
         <HtmlContent src="/content/main.html" />
-        <div className="mx-auto mt-16 space-y-4">
+        <div className="mx-auto mt-6 space-y-4 md:mt-16">
           <Typography variant={"h4"} as={"h2"}>
             Vælg din kommune for at gå til det nye GO
           </Typography>
